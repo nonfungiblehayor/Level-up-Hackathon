a12d0214334062006dec339d2d29db30fc387114
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2023-11-27 12:01:55 +0000

    feat: functionalities

--- app.js
+++ app.js
@@ -1,9 +1,59 @@
-const cancelBtn = document.getElementsByClassName('cancel-svg');
 const planPopUp = document.getElementsByClassName('plan_option')
+const arrowDown = document.getElementsByClassName('arrow-down')
+const arrowUp = document.getElementsByClassName('arrow-up')
 
-// cancelBtn.addEventListener('click', function() {
-//     planPopUp.style.display = 'none'
-// })
 const closePlanPopup = () => {
     planPopUp[0].style.display = 'none'
+}
+
+const showArrowUp = () => {
+    arrowDown[0].style.display = 'none'
+    arrowUp[0].style.display = 'flex'
+}
+
+const showArrowDown = () => {
+    arrowDown[0].style.display = 'flex'
+    arrowUp[0].style.display = 'none'
+}
+
+const showPopup = (popup) => {
+    const targetPopup = document.getElementsByClassName(popup)
+    if(targetPopup[0].style.display === '') {
+        targetPopup[0].style.display = 'flex'
+    } else if(targetPopup[0].style.display === 'flex') {
+        targetPopup[0].style.display = 'none'
+    } else if(targetPopup[0].style.display = 'none') {
+        targetPopup[0].style.display = 'flex'
+    }
+}
+
+const showPanel = (panel) => {
+    const target = document.getElementsByClassName('setup-content')
+    for(const item of target) {
+        if (item.style.display === 'flex') {
+            item.style.display = 'none'
+        }
+    }
+    target[panel].style.display = 'flex'
+}
+
+const markPanel = (nextPanel, currentPanel, event) => {
+    const mark = document.getElementsByClassName('mark')
+    const unmark = document.getElementsByClassName('unmark')
+    const target = document.getElementsByClassName('setup-content')
+    event.stopPropagation()
+    if(unmark[currentPanel].style.display === '') {
+        mark[currentPanel].style.display = 'flex'
+        unmark[currentPanel].style.display = 'none'
+        target[nextPanel].style.display = 'flex'
+        target[currentPanel].style.display = 'none'
+        if(currentPanel === 4) {
+            target[currentPanel].style.display = 'flex'
+        }
+    } else if (mark[currentPanel].style.display === 'flex') {
+        mark[currentPanel].style.display = 'none'
+        unmark[currentPanel].style.display = 'flex'
+        target[nextPanel].style.display = 'none'
+        target[currentPanel].style.display = 'flex'
+    }
 }
